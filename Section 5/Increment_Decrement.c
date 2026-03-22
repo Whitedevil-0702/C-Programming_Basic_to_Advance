becb574339a80aa068f0fbf7a3f3707059b75159
Whitedevil-0702 <jadhavhsj@gmail.com>
2026-03-23 03:43:38 +0530

    Increment & Decrement Operators

---
+++ Section 5/Increment_Decrement.c
@@ -0,0 +1,32 @@
+/// WAP to understand Increment and Decrement Operators in C
+//increment(++) and Decrement(--) Operators
+
+#include<stdio.h>
+int main()
+{
+    //Increment x=x+1;x++;
+
+    //Decrement x=x-1;  x--;
+
+    //prefix ++x --x
+   /* int x=8;
+    printf("x=%d\n",x);
+    printf("x=%d\n",++x);
+    printf("x=%d\n",x);
+    printf("x=%d\n",--x);
+    printf("x=%d\n",x);*/
+
+    //postfix x++ x--
+    int x=8;
+    printf("x= %d\n",x);
+    printf("x= %d\n",x++);
+    printf("x= %d\n",x);
+    printf("x= %d\n",x--);
+    printf("x= %d\n",x);
+
+
+
+
+
+    return 0;
+}
