5e9156d511bf2197409a39d6f57701cc0bda2e51
Whitedevil-0702 <jadhavhsj@gmail.com>
2026-03-23 02:54:24 +0530

    Section 4 Updated files

---
+++ Section 4/Conversion_specifications.c
@@ -0,0 +1,13 @@
+///Convsersion Specification
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main() {
+    int x = 256;
+    double y = 23.123456789;
+    char str[] = "Hello, World!";
+    printf("Decimal: %d\n",x);  // Decimal Format
+    printf("Float: %f\n",y);   // Float Format
+    printf("String: %s\n",str); // String Format
+    return 0;
+}
